7d8cbf387a2ed789236ff05538dbbe761d85205b
Mikołaj Klonowski <nick-93@o2.pl>
2018-06-19 16:31:45 +0200

    Zadanie 6.3 / 18 poprawa

--- src/ArrayComparator.java
+++ src/ArrayComparator.java
@@ -4,8 +4,9 @@ public class ArrayComparator {
     boolean compare(int[][] x, int[][] y) {
         boolean a = true;
         if (x.length != y.length || x[0].length != y[0].length)
-            a = false;
-        for (int i = 0; i < x.length; i++) {
+            return  false;
+        for (int i = 0; i < x.length ; i++) {
+
             for (int j = 0; j < x[i].length; j++) {
                 if (x[i][j] != y[i][j]) {
                     a = false;
